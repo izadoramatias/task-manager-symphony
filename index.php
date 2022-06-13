419f4b9cc80feb3984b63e71679efe8883862927
izadora matias <contato.izadoramatias@gmail.com>
2022-06-13 19:06:24 -0300

    basic structure application

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Task Manager</title>
+</head>
+<body>
+    
+    <div class="container">
+        <div class="header">
+            <header>
+                <h1>Task Manager</h1>
+            </header>
+        </div>
+
+        <div class="form">
+            <form action="" method="GET">
+
+                <label for="task_name"></label>
+                <input type="text" name="task_name" placeholder="Nome da tarefa" id="task_name">
+                <button type="submit">Cadastrar</button>
+
+            </form>
+        </div>
+
+        <div class="separator"></div>
+
+        <div class="list-tasks">
+
+            <ul>
+                <li>Tarefa 1</li>
+                <li>Tarefa 2</li>
+                <li>Tarefa 3</li>
+            </ul>
+
+        </div>
+
+        <div class="footer">
+
+            <footer>
+                <p>Developed by @izadoramatias</p>
+            </footer>
+
+        </div>
+    
+    </div>
+
+</body>
+</html>
